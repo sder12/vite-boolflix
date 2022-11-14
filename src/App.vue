81f4ababa132ad9b10ec5e6b4945a243186f7e05
sder12 <sara.cetto@gmail.com>
2022-11-14 11:50:17 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import { store } from "./store"
+export default {
+  data() {
+    return {
+      store
+    }
+  }
+}
+</script>
+ 
+<template>
+  <h1> Movies & Series Tv</h1>
+  <h2>{{ store.debug }} </h2>
+</template>
+ 
+<style lang="scss">
+
+</style>
