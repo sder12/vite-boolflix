1f5c9019bffbc3ad5f6c42d21ef5f5c1d543dce1
sder12 <sara.cetto@gmail.com>
2022-11-15 17:16:21 +0100

    Styling search bar

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -28,11 +28,24 @@ export default {
 <template>
 
     <label for="search" class="sr-only">Search a movie or a series TV</label>
-    <input type="search" id="search" placeholder="Search a movie" v-model="input" @keyup.enter="saveInput()">
+    <input type="search" id="search" placeholder="Search a movie - series" v-model="input" @keyup.enter="saveInput()">
 
     <button class="btn btn-search" @click="saveInput()"> Search </button>
 </template>
  
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
 
+#search {
+    padding: .5em 1em;
+    border-radius: 6px;
+    margin-right: 10px;
+}
+
+.btn-search {
+    background-color: $logo-color;
+    color: white;
+    padding: .5em 2em;
+    border-radius: 15px;
+}
 </style>
